a3eb6e1975119873f1713f23a033842c874d1453
Szymon Katarzynski <katarzynski.s@pg.com>
2022-08-01 13:07:17 +0200

    Initial commit

---
+++ src/main/java/com/cloud/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
